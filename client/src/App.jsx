9fa0bd9d6c0bb5f41eccf01e0076468dd24e657a
ansarkhan26 <ansarkhan260205@gmail.com>
2026-01-27 20:51:46 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
